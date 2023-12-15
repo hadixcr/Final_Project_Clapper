c8150806881ea131b2b19aefc9fe59245083bcc4
hadixcr <150840354+hadixcr@users.noreply.github.com>
2023-12-14 22:57:52 -0500

    Update counter.v
    Added comments

--- main_files/counter.v
+++ main_files/counter.v
@@ -8,11 +8,18 @@ module counter(
   reg [3:0] count;
   reg ledPeakPrev;
 
-  always @(posedge clk or negedge rst) begin
-    if (rst == 1'b0) begin
+  always @(posedge clk or posedge rst) 
+  begin
+
+    //This resets the count when the rest of the program reconfigures.
+    if (rst) begin
       count <= 4'b0000;
     end
-    else if (ledPeak == 1'b1 && ledPeakPrev == 1'b0) begin
+
+    //Detects if the led is currently peaking and the last instance of it is not peaking.
+    //This determines whether a clap is happening, updating the counter to add one to the current total.
+    else if (ledPeak == 1'b1 && ledPeakPrev == 1'b0) 
+    begin
       count <= count + 1'b1;
     end
     ledPeakPrev <= ledPeak;
