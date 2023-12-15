0b4d4079d6d2f2128bed4a108c690da13bc90fac
hadixcr <150840354+hadixcr@users.noreply.github.com>
2023-12-14 23:03:49 -0500

    Update clapCounter.v

--- main_files/clapCounter.v
+++ main_files/clapCounter.v
@@ -8,15 +8,19 @@ module clapCounter (
 
   always @(posedge clk or posedge rst) 
   begin
-  
-   if (rst)
-	begin
-		countDisplay <= 4'b0000;
+
+   	//Checks if the program reconfigured, reseting the count to zero to restart counting.  
+	if (rst)
+   	begin
+   		countDisplay <= 4'b0000;
 	end
-	else if (fftInDataAbs[15] == 1'b1)
-	begin
+
+	//Checks if the data reaches the 15th bit, reflecting the last led on the board lighting up.
+	//Using this, we determine there was a clap so we add one to the current sum output.
+   	else if (fftInDataAbs[15] == 1'b1)
+   	begin
 		countDisplay <= countDisplay + 1'b1;
-	end
+  	end
   end
 
 endmodule
